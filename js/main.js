94f8ffe045ae39550e5d76e24a13af8bc0da7898
bonniecampbell <bonnie.jp.campbell@hotmail.com>
2016-03-18 18:46:01 -0400

    fixed the mistakes from Markbot

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var $input = $('#to-do-list-input');
-var $list = $(".list");
+var $list = $('.list');
 
 $('form').on('submit', function (e) {
+
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+  $input.val('');
 });
 
 $('ul').on('click', 'li', function (e) {
